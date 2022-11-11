bc34964d033aaee02752e8b2d0e764d2978c70f1
vnikonov63 <nikonov.va@yandex.ru>
2022-11-10 16:05:25 -0800

    sonme small script to grade the submission of the student]

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,28 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+
+// added the implementation of the StringChecker
+class CheckLength implements StringChecker {
+  public boolean checkString(String s) {
+    return s.length() >= 3;
+  }
+}
 
 public class TestListExamples {
-  // Write your grading tests here!
+  @Test
+  public void testFilterGeneral() {
+    List<String> input = Arrays.asList("lolo", "lol", "lilloo", "pi", "ro", "g");
+    StringChecker sc = new CheckLength();
+    List<String> expectedOutput = Arrays.asList("lolo", "lol", "lilloo");
+    assertEquals(expectedOutput, ListExamples.filter(input, sc));
+  }
+
+  @Test
+  public void mergeGeneral() {
+    List<String> input1 = Arrays.asList("a", "c", "e", "g");
+    List<String> input2 = Arrays.asList("b", "d", "f", "h");
+    List<String> expectedOutput = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
+    assertEquals(expectedOutput, ListExamples.merge(input1, input2));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,44 @@
 # Create your grading script here
+# set -e
+DIRNAME="./student-submission/"
 
-set -e
+rm -rf $DIRNAME
+git clone $1 $DIRNAME
 
-rm -rf student-submission
-git clone $1 student-submission
+MUSTFILE="ListExamples.java"
+MAXIMUMCREDIT=5
+SOURCETEST="TestListExamples.java"
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
 
+# Checking the existance of the file
+if [ -f $DIRNAME$MUSTFILE ]
+then
+    echo -e "The file" $MUSTFILE "in the directory " $DIRNAME "is present [+1 point]"
+else 
+    echo -e "The file" $MUSTFILE "in the directory " $DIRNAME "is not present [+0 points]"
+    exit
+fi
+
+cp $SOURCETEST $DIRNAME
+cd $DIRNAME
+javac -cp $CPATH *.java 2> compile-err.txt
+
+# Checking the compilation of the file
+if [ $? -eq 0 ]
+then
+    echo "The compilation of the directory" $DIRNAME "was successfull [+1 point]"
+else 
+    echo "The compilation of the directory" $DIRNAME "failed [+0 points]"
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore $SOURCETEST > test-err.txt
+
+# Checking the correctness of the file
+if [ $? -eq 0 ]
+then
+    echo "All the tests within" $SOURCETEST "were successfull [+1 point]"
+else 
+    echo "Some the tests within" $SOURCETEST "were not successfull [+0 points]"
+    grep "failure" test-err.txt
+fi
